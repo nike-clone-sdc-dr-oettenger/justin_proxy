8e3ae7b3c22c6d50555c013c921c532e1cb0e137
Justin O'Hara <justinrobertohara@gmail.com>
2019-08-20 20:56:10 -0700

    shell of proxy server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const proxy = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const port = 6969;
+proxy.use(bodyParser.json());
 
-proxy.listen(port, () => console.log(`Example app listening on port ${port}!`));
+proxy.listen(port, () =>
+  console.log(`Proxy Server listening on port ${port}!`)
+);
+
+proxy.get('/api/reviews', (req, res) => {
+  axios({
+    method: 'get',
+    url: 'http://127.0.0.1:3000/api/reviews'
+  });
+});
+
+//for potential shoe info from marcus's database
+
+// proxy.get('/shoes', (req, res) => {
+//   axios({
+//     method: 'get',
+//     url: 'http://127.0.0.1:1128/api/reviews'
+//   });
+// });
