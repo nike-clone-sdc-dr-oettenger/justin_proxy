4039126c1cf37f83831e13ccf6b2b888bacc548c
Justin O'Hara <justinrobertohara@gmail.com>
2019-09-16 16:57:41 -0700

    fixed server code

--- server/index.js
+++ server/index.js
@@ -1,12 +1,55 @@
 const express = require('express');
+const request = require('request');
 const proxy = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.PORT || 6969;
+const axios = require('axios');
 
 proxy.use(cors());
 proxy.use(bodyParser.json());
 proxy.use(express.static('client'));
+proxy.use(bodyParser.urlencoded({ extended: true }));
+
+const reviewsUrl = 'http://localhost:3000/api/reviews';
+
+proxy.get('/api/reviews', (req, res) => {
+  // res.status(200).send('endpoint');
+
+  console.log(req.query.shoe_id, 'shoe id');
+  console.log('request going to microservice');
+  axios
+    .get(reviewsUrl, {
+      params: {
+        shoe_id: req.query.shoe_id
+      }
+    })
+    .then(response => {
+      console.log('response data', response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+// proxy.all('/api/reviews', (req, res) => {
+//   console.log(`Redirecting request to ${reviewsUrl}.`);
+//   console.log(req.body);
+//   let options = {
+//     url: `${reviewsUrl}`,
+//     method: req.method,
+//     qs: req.query
+//   };
+//   request(options, (err, response) => {
+//     if (err) {
+//       alert(err);
+//     }
+//     let body = JSON.parse(response.body);
+//     res.status(200).send(body);
+//   });
+
+//   // axios.get('/');
+// });
 
 proxy.listen(port, () =>
   console.log(`Proxy Server listening on port ${port}!`)
